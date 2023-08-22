cfb80c1396848c46c05bd61467cd7efc543b823b
zidanindratama <zidanindratama31@gmail.com>
2023-08-22 07:56:58 +0700

    4. starting the classy weather app

--- src/App.jsx
+++ src/App.jsx
@@ -2,9 +2,34 @@ import React from "react";
 import Counter from "./components/Counter";
 
 class App extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = { location: "Jakarta" };
+    this.fetchWeather = this.fetchWeather.bind(this);
+  }
+
+  fetchWeather = () => {
+    console.log("Loading data...");
+    console.log(this);
+  };
+
   render() {
-    return <Counter />;
+    return (
+      <div className="app">
+        <h1>Classy Weather</h1>
+        <div>
+          <input
+            type="text"
+            placeholder="Search for location..."
+            value={this.state.location}
+            onChange={(e) => this.setState({ location: e.target.value })}
+          />
+        </div>
+        <button onClick={this.fetchWeather}>Get Weather</button>
+      </div>
+    );
   }
 }
 
-export default Counter;
+export default App;
